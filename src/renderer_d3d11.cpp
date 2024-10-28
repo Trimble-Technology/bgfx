0a5d8fc60140b34a8739b8b3e47eb7da0452da1c
Fredrik Kullenberg <114995021+fkullenberg@users.noreply.github.com>
2024-10-28 18:18:52 +0200

    Commented out shader filtering and patching of UAV
    Removed UAV filtering and patching since it produces a different binary output even if nothing was filtered out. Without this there is a crash loading CreatorCore::BgfxView::CompassSymbol fragment shader.

--- src/renderer_d3d11.cpp
+++ src/renderer_d3d11.cpp
@@ -4222,42 +4222,34 @@ namespace bgfx { namespace d3d11
 		bx::skip(&reader, shaderSize+1);
 
 		const Memory* temp = NULL;
-
+		
+		/*
+		!! COMMENTED OUT DUE TO CRASH WITH COMPASSSYMBOL !!
+   
 		if (!isShaderType(magic, 'C'))
 		{
 			bx::MemoryReader rd(code, shaderSize);
-
+			
 			DxbcContext dxbc;
 			read(&rd, dxbc, bx::ErrorAssert{});
-
-			/*
-			!! COMMENTED OUT DUE TO CRASH WITH COMPASSSYMBOL !!
 			
-			if (!isShaderType(magic, 'C'))
+			bool patchShader = !dxbc.shader.aon9;
+			if (patchShader)
 			{
-				bx::MemoryReader rd(code, shaderSize);
+				union { uint32_t offset; void* ptr; } cast = { 0 };
+				filter(dxbc.shader, dxbc.shader, patchUAVRegisterByteCode, cast.ptr);
+				patchUAVRegisterDebugInfo(dxbc.spdb);
 				
-				DxbcContext dxbc;
-				read(&rd, dxbc, bx::ErrorAssert{});
+				temp = alloc(shaderSize);
+				bx::StaticMemoryBlockWriter wr(temp->data, temp->size);
 				
-				bool patchShader = !dxbc.shader.aon9;
-				if (patchShader)
-				{
-					union { uint32_t offset; void* ptr; } cast = { 0 };
-					filter(dxbc.shader, dxbc.shader, patchUAVRegisterByteCode, cast.ptr);
-					patchUAVRegisterDebugInfo(dxbc.spdb);
-					
-					temp = alloc(shaderSize);
-					bx::StaticMemoryBlockWriter wr(temp->data, temp->size);
-					
-					int32_t size = write(&wr, dxbc, &err);
-					dxbcHash(temp->data + 20, size - 20, temp->data + 4);
-					
-					code = temp->data;
-				}
+				int32_t size = write(&wr, dxbc, &err);
+				dxbcHash(temp->data + 20, size - 20, temp->data + 4);
+				
+				code = temp->data;
 			}
-			*/
 		}
+		*/
 
 		if (isShaderType(magic, 'F') )
 		{
